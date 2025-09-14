54a46c69a82e0b4ed08f0608998a9898479fdd53
mainak55512 <mbhattacharjee432@gmail.com>
2025-09-15 00:44:19 +0530

    fourth commit

--- array_methods.go
+++ array_methods.go
@@ -1,30 +1,46 @@
 package ccl
 
+// ForEach function for Array
+func ForEach[T comparable](array []T, callback func(element T)) {
+	for _, value := range array {
+		callback(value)
+	}
+}
+
 // Map function for Array
 func Map[T comparable](array []T, callback func(element T) T) []T {
 	var newArr []T
-	for _, value := range array {
-		newArr = append(newArr, callback(value))
-	}
+	ForEach(array, func(e T) {
+		newArr = append(newArr, callback(e))
+	})
 	return newArr
 }
 
 // Filter function for Array
 func Filter[T comparable](array []T, callback func(element T) bool) []T {
 	var newArr []T
-	for _, value := range array {
-		if callback(value) {
-			newArr = append(newArr, value)
+	ForEach(array, func(e T) {
+		if callback(e) {
+			newArr = append(newArr, e)
 		}
-	}
+	})
 	return newArr
 }
 
 // Reduce function for Array
 func Reduce[T, K comparable](array []T, callback func(accumulator K, element T) K, initial K) K {
 	var acc = initial
-	for _, value := range array {
-		acc = callback(acc, value)
-	}
+	ForEach(array, func(e T) {
+		acc = callback(acc, e)
+	})
 	return acc
+}
+
+// Reverse function for Array
+func Reverse[T comparable](array []T) []T {
+	var newArr []T
+	for i := len(array) - 1; i >= 0; i-- {
+		newArr = append(newArr, array[i])
+	}
+	return newArr
 }
